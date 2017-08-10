a53efb8f9833459dd2cf364fceda02e7f132f144
Daniel Tan <danieltan843@gmail.com>
2017-08-10 22:42:44 +1000

    Worked on Task 1

---
+++ Assets/DannyRealm/Scripts/CustomTransform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CustomTransform : MonoBehaviour {
+
+	public Vector3 Position { get; set; }
+	public float Rotation { get; set; }
+	public float Scale { get; set; }
+
+	public void Translate(Vector3 displacement) {
+		Position += displacement;
+	}
+
+	public void Rotate(float angle) {
+		Rotation += angle;
+	}
+}
--- Assets/DannyRealm/Scripts/RenderObject.cs
+++ Assets/DannyRealm/Scripts/RenderObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RenderObject : MonoBehaviour {
+public class RenderObject : CustomTransform {
 
 	Mesh mesh;
 	public Material mat;
@@ -12,6 +12,15 @@ public class RenderObject : MonoBehaviour {
 	const int minSegments = 3;
 	public float radius = 1f;
 
+	[Header("Transformations")]
+	public float translateSpeed = 2f;
+	float rotationSpeed = -Mathf.PI / 2f;
+
+	[Header("Task1")]
+	public Vector3 pos1;
+	public Vector3 pos2;
+	float moveTimer = 0f;
+	float moveTime = 3f;
 
 	void Start() {
 		gameObject.AddComponent<MeshRenderer> ();
@@ -20,26 +29,9 @@ public class RenderObject : MonoBehaviour {
 		mesh = GetComponent<MeshFilter> ().mesh;
 		GetComponent<MeshRenderer> ().material = mat;
 
-		if (segments < minSegments)
-			segments = minSegments;
+		Scale = radius;
+		Position = pos1;
 
-		Vector3[] verts = new Vector3[segments * 3];
-
-		float angle = 2 * Mathf.PI / (float)segments;
-
-		for (int i = 0; i < verts.Length; i += 3) {
-			verts [i] = Vector3.zero;
-			verts [i + 1] = new Vector3 (Mathf.Cos (i/3 * angle), Mathf.Sin (i/3 * angle), 0f) * radius;
-			verts [i + 2] = new Vector3 (Mathf.Cos (i/3 * angle + angle), Mathf.Sin (i/3 * angle + angle), 0f) * radius;
-			Debug.Log ("Tri: " + verts [i] + " | " + verts [i + 1] + " | " + verts [i + 2]);
-//			Debug.Log ("Created Vert");
-		}
-
-		int[] tris = new int[verts.Length];
-
-		for (int i = 0; i < tris.Length; i++) {
-			tris [i] = i;
-		}
 
 //		verts [0] = Vector3.zero;
 //		verts [1] = Vector3.right;
@@ -47,8 +39,6 @@ public class RenderObject : MonoBehaviour {
 //		verts [3] = new Vector3 (Mathf.Cos (2 * Mathf.PI / 3f * 2), Mathf.Sin (2 * Mathf.PI / 3f * 2), 0f) + Vector3.up * 3;
 //		verts [4] = new Vector3 (Mathf.Cos (2 * Mathf.PI / 3f * 3), Mathf.Sin (2 * Mathf.PI / 3f * 3), 0f) + Vector3.up * 3;
 //		verts [5] = new Vector3 (Mathf.Cos (2 * Mathf.PI / 3f * 4), Mathf.Sin (2 * Mathf.PI / 3f * 4), 0f) + Vector3.up * 3;
-
-		mesh.vertices = verts;
 
 //		mesh.colors = new Color[] {
 //			Color.white,
@@ -59,10 +49,44 @@ public class RenderObject : MonoBehaviour {
 //			Color.black
 //		};
 //
-		mesh.triangles = tris;
+
 	}
 
 	void Update() {
+		Translate (Vector3.right * translateSpeed * Time.deltaTime);
+		Rotate (rotationSpeed * Time.deltaTime);
+
+		DrawShape ();
+	}
+
+	void DrawShape() {
+
+		if (segments < minSegments)
+			segments = minSegments;
+
+		Vector3[] verts = new Vector3[segments * 3];
+
+		float angle = 2 * Mathf.PI / (float)segments;
+
+		for (int i = 0; i < verts.Length; i += 3) {
+			verts [i] = Vector3.zero + this.Position;
+			verts [i + 1] = new Vector3 (Mathf.Cos (i / 3 * angle + Rotation), Mathf.Sin (i / 3 * angle + Rotation), 0f) * Scale + this.Position;
+			verts [i + 2] = new Vector3 (Mathf.Cos (i/3 * angle + angle + Rotation), Mathf.Sin (i/3 * angle + angle + Rotation), 0f) * Scale + this.Position;
+//			Debug.Log ("Tri: " + verts [i] + " | " + verts [i + 1] + " | " + verts [i + 2]);
+		}
+
+		int[] tris = new int[verts.Length];
+
+		for (int i = 0; i < tris.Length; i++) {
+			tris [i] = i;
+		}
+
+		mesh.vertices = verts;
+
+		mesh.triangles = tris;
+	}
+
+	void BetweenTwoPoints() {
 		
 	}
 }
